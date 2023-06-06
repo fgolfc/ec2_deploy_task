7c9020219854637ad200f8601343a18912a949ea
fgolfc <cf.ts.fujimoto@gmail.com>
2023-06-06 20:18:04 +0900

    key repar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_085415) do
+ActiveRecord::Schema.define(version: 2023_05_27_111733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
