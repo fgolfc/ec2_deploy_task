112e7061fe4078dcc51c26477f106ae8de5a9e8e
fgolfc <cf.ts.fujimoto@gmail.com>
2023-06-08 22:44:39 +0900

    image delete

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -64,6 +64,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :content, :thumbnail, :image)
+      params.require(:blog).permit(:title, :content, :thumbnail)
     end
 end
